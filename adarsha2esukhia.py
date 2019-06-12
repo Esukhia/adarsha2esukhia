6e5337d269aec7b04d57de12d6e386fb80f5ef70
Elie Roux <elie.roux@telecom-bretagne.eu>
2019-06-12 17:10:25 +0200

    add Unicode normalization

--- adarsha2esukhia.py
+++ adarsha2esukhia.py
@@ -64,6 +64,30 @@ def testUrl(work, pbs):
         status = True
     return status
 
+def normalizeUni(strNFC):
+    strNFC = strNFC.replace("\u0F00", "\u0F68\u0F7C\u0F7E") # ༀ
+    strNFC = strNFC.replace("\u0F43", "\u0F42\u0FB7") # གྷ
+    strNFC = strNFC.replace("\u0F48", "\u0F47\u0FB7") # ཈
+    strNFC = strNFC.replace("\u0F4D", "\u0F4C\u0FB7") # ཌྷ
+    strNFC = strNFC.replace("\u0F52", "\u0F51\u0FB7") # དྷ
+    strNFC = strNFC.replace("\u0F57", "\u0F56\u0FB7") # བྷ
+    strNFC = strNFC.replace("\u0F5C", "\u0F5B\u0FB7") # ཛྷ
+    strNFC = strNFC.replace("\u0F69", "\u0F40\u0FB5") # ཀྵ
+    strNFC = strNFC.replace("\u0F73", "\u0F71\u0F72") # ཱི
+    strNFC = strNFC.replace("\u0F75", "\u0F71\u0F74") #  ཱུ
+    strNFC = strNFC.replace("\u0F76", "\u0FB2\u0F80") # ྲྀ
+    strNFC = strNFC.replace("\u0F77", "\u0FB2\u0F71\u0F80") # ཷ
+    strNFC = strNFC.replace("\u0F78", "\u0FB3\u0F80") # ླྀ
+    strNFC = strNFC.replace("\u0F79", "\u0FB3\u0F71\u0F80") # ཹ
+    strNFC = strNFC.replace("\u0F81", "\u0F71\u0F80") #  ཱྀ
+    strNFC = strNFC.replace("\u0F93", "\u0F92\u0FB7") # ྒྷ
+    strNFC = strNFC.replace("\u0F9D", "\u0F9C\u0FB7") # ྜྷ
+    strNFC = strNFC.replace("\u0FA2", "\u0FA1\u0FB7") # ྡྷ
+    strNFC = strNFC.replace("\u0FA7", "\u0FA6\u0FB7") # ྦྷ
+    strNFC = strNFC.replace("\u0FAC", "\u0FAB\u0FB7") # ྫྷ
+    strNFC = strNFC.replace("\u0FB9", "\u0F90\u0FB5") # ྐྵ
+    return strNFC
+
 def getwork(work):
     # put the work in a json
     i = work[1]
@@ -72,8 +96,8 @@ def getwork(work):
         url = apiBase.format(name=work[0], pbs=i)
         response = requests.get(url)
         text = response.text.replace("},{", "},\n{")
+        text = normalizeUni(text)
         pages = text.splitlines()
-
         for page in pages:
             writePage(page)
         print(f'pbs: {i}')
